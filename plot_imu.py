901e7c6a93fafaaf274a5f2cf5196e668c1636f0
sal <salehahr@users.noreply.github.com>
2021-04-22 12:35:43 +0200

    modified plot_imu/plot_recon....py to plot both mono and gt trajectories

--- plot_imu.py
+++ plot_imu.py
@@ -8,21 +8,33 @@ num_imu_between_frames = 30
 stereoGT_traj = VisualTraj("stereoGT",
         "./trajs/offline_mandala0_gt.txt",
         cap=max_vals)
+mono_traj = VisualTraj("mono",
+        "./trajs/offline_mandala0_mono.txt",
+        cap=max_vals)
 
 imu_covariance = [0.01, 0.01, 0.01, 0.07, 0.005, 0.1]
-imu_traj = ImuTraj(name='imu gt',
-        filepath="./trajs/mandala0_imu.txt",
+imu_gt_traj = ImuTraj(name='imu gt',
+        filepath="./trajs/mandala0_imu_gt.txt",
         vis_data=stereoGT_traj,
+        cap=max_vals,
+        num_imu_between_frames=num_imu_between_frames,
+        covariance=imu_covariance)
+imu_mono_traj = ImuTraj(name='imu mono',
+        filepath="./trajs/mandala0_imu_mono.txt",
+        vis_data=mono_traj,
         cap=max_vals,
         num_imu_between_frames=num_imu_between_frames,
         covariance=imu_covariance)
 
 # # plot noisy imu and non-noisy imu
-min_t = imu_traj.t[0]
-max_t = imu_traj.t[-1]
+min_t = imu_gt_traj.t[0]
+max_t = imu_gt_traj.t[-1]
 
-axes = imu_traj.noisy.plot(min_t=min_t, max_t=max_t)
-axes = imu_traj.plot(axes, min_t=min_t, max_t=max_t)
+gt_axes = imu_gt_traj.noisy.plot(min_t=min_t, max_t=max_t)
+gt_axes = imu_gt_traj.plot(gt_axes, min_t=min_t, max_t=max_t)
+
+mono_axes = imu_mono_traj.noisy.plot(min_t=min_t, max_t=max_t)
+mono_axes = imu_mono_traj.plot(mono_axes, min_t=min_t, max_t=max_t)
 
 plt.legend()
 plt.show()
--- plot_reconstructed.py
+++ plot_reconstructed.py
@@ -8,18 +8,27 @@ num_imu_between_frames = 30
 stereoGT_traj = VisualTraj("stereoGT",
         "./trajs/offline_mandala0_gt.txt",
         cap=max_vals)
+mono_traj = VisualTraj("mono",
+        "./trajs/offline_mandala0_mono.txt",
+        cap=max_vals)
 
 imu_covariance = [0.01, 0.01, 0.01, 0.07, 0.005, 0.1]
-imu_traj = ImuTraj(name='imu gt',
-        filepath="./trajs/mandala0_imu.txt",
+imu_gt_traj = ImuTraj(name='imu gt',
+        filepath="./trajs/mandala0_imu_gt.txt",
         vis_data=stereoGT_traj,
+        cap=max_vals,
+        num_imu_between_frames=num_imu_between_frames,
+        covariance=imu_covariance)
+imu_mono_traj = ImuTraj(name='imu mono',
+        filepath="./trajs/mandala0_imu_mono.txt",
+        vis_data=mono_traj,
         cap=max_vals,
         num_imu_between_frames=num_imu_between_frames,
         covariance=imu_covariance)
 
 # plots
-min_t = imu_traj.t[0]
-max_t = imu_traj.t[-1]
+min_t = imu_gt_traj.t[0]
+max_t = imu_gt_traj.t[-1]
 
 # # sanity check -- plot interpolated data
 # stereoGT_traj.interpolate(num_imu_between_frames)
@@ -27,9 +36,13 @@ max_t = imu_traj.t[-1]
 # axes = stereoGT_traj.interpolated.plot(axes, min_t=min_t, max_t=max_t)
 
 # sanity check -- plot reconstructed trajectory from imu
-reconstructed = imu_gt_traj.reconstruct_vis_traj()
-recon_axes = stereoGT_traj.plot(min_t=min_t, max_t=max_t)
-recon_axes = reconstructed.plot(recon_axes, min_t=min_t, max_t=max_t)
+recon_gt = imu_gt_traj.reconstruct_vis_traj()
+recon_gt_axes = stereoGT_traj.plot(min_t=min_t, max_t=max_t)
+recon_gt.plot(recon_gt_axes, min_t=min_t, max_t=max_t)
+
+recon_mono = imu_mono_traj.reconstruct_vis_traj()
+recon_mono_axes = mono_traj.plot(min_t=min_t, max_t=max_t)
+recon_mono.plot(recon_mono_axes, min_t=min_t, max_t=max_t)
 
 plt.legend()
 plt.show()
