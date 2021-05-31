ff4550451ef15ec8b1f5a4d66f29a6d46767c37d
sal <salehahr@users.noreply.github.com>
2021-05-31 12:46:08 +0200

    3d plots in plot_recon

--- tests/plot_reconstructed.py
+++ tests/plot_reconstructed.py
@@ -10,16 +10,23 @@ from generate_data import stereoGT_traj, mono_traj, imu_gt_traj, imu_mono_traj, 
 # axes = stereoGT_traj.plot(min_t=min_t, max_t=max_t)
 # axes = stereoGT_traj.interpolated.plot(axes, min_t=min_t, max_t=max_t)
 
-# sanity check -- plot reconstructed trajectory from imu
+# # sanity check -- plot reconstructed trajectory from imu (coordinates)
 recon_gt = imu_gt_traj.reconstruct_vis_traj()
 recon_gt_axes = stereoGT_traj.plot(min_t=min_t, max_t=max_t)
-recon_gt.plot(recon_gt_axes, min_t=min_t, max_t=max_t)
+recon_gt_axes = recon_gt.plot(recon_gt_axes, min_t=min_t, max_t=max_t)
 
 recon_mono = imu_mono_traj.reconstruct_vis_traj()
 recon_mono_axes = mono_traj.plot(min_t=min_t, max_t=max_t)
 recon_mono.plot(recon_mono_axes, min_t=min_t, max_t=max_t)
 
-# plot velocities
+# sanity check -- plot reconstructed trajectory from imu (3D)
+recon_gt_axes = stereoGT_traj.plot_3d()
+recon_gt_axes = recon_gt.plot_3d(ax=recon_gt_axes)
+
+recon_mono_axes = imu_mono_traj.plot_3d()
+recon_mono_axes = recon_gt.plot_3d(ax=recon_mono_axes)
+
+# sanity check -- plot velocities
 v_axes = None
 v_axes = stereoGT_traj.plot_velocities(v_axes, min_t=min_t, max_t=max_t)
 v_axes = imu_gt_traj.plot_velocities(v_axes, min_t=min_t, max_t=max_t)
