56da96bcd7367974ebf7f43eb933caef31b43f72
sal <salehahr@users.noreply.github.com>
2021-07-23 11:38:12 +0200

    added submodule feudalism/spatialmath-python

---
+++ upgrade.sh
@@ -0,0 +1,2 @@
+#py -m pip install -e git+https://github.com/feudalism/spatialmath-python.git#egg=f-spatialmath --upgrade
+python3 -m pip install -e git+ssh://git@github-feudalism/feudalism/spatialmath-python.git#egg=f-spatialmath --upgrade
