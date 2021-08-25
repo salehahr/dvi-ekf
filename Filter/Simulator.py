1ec076ff149f6ddf23d8c6f328f6e329aaa3a8aa
sal <salehahr@users.noreply.github.com>
2021-08-25 20:28:12 +0200

    implemented basin hopping

--- Filter/Simulator.py
+++ Filter/Simulator.py
@@ -1,4 +1,5 @@
 import copy
+from scipy.optimize import basinhopping
 
 class Simulator(object):
     def __init__(self, config):
@@ -7,23 +8,31 @@ class Simulator(object):
         _, camera, imu = config.init_filter_objects()
         self.x0, self.cov0        = config.get_IC(imu, camera)
 
+        # optim params
+        self._kp = config.scale_process_noise
+
         # simulation run params
         self.num_kf_runs = config.num_kf_runs
         self.cap_t      = config.cap_t
-        
+
         # results
         self.dof_mses = []
         self.dof_mse_best  = 1e10
+        self.dof_mse_avg = None
         self.kf_best = None
 
     @property
-    def dof_mse(self):
-        return sum(self.dof_mses) / len(self.dof_mses)
+    def kp(self):
+        return self._kp
+    @kp.setter
+    def kp(self, val):
+        self._kp = val
+        self.config.scale_process_noise = val
 
     @property
     def best_run_id(self):
         return self.kf_best.run_id
-    
+
     def run(self, kf, camera):
         for k in range(self.num_kf_runs):
             run_id = k + 1
@@ -40,6 +49,32 @@ class Simulator(object):
 
             # reset for next run
             kf.reset(self.config, self.x0, self.cov0)
+
+        self.dof_mse_avg = sum(self.dof_mses) / len(self.dof_mses)
+        self.dof_mses = [] # clear
+
+    def optimise(self, kf ,camera):
+        def optim_func(kp):
+            self.kp = kp
+            self.run(kf, camera)
+            return self.dof_mse_avg
+
+        def print_fun(x, f, accepted):
+            print(f"for kp {x[0]:.3E}: average MSE {f:.4f} accepted {int(accepted)}")
+
+        kp0 = 0.0010
+        # global minimum: kp = 4.024E-01, mse = 4.996
+        minimizer_kwargs = {"method": "BFGS"}
+
+        ret = basinhopping(optim_func, kp0,
+                            minimizer_kwargs=minimizer_kwargs,
+                            callback=print_fun,
+                            niter=1)
+
+        # results
+        print(f"global minimum: kp = {ret.x[0]:.3E}, mse = {ret.fun:.3f}")
+        self.best_kp = ret.x
+        self.dof_mse = ret.fun
 
     def save_and_plot(self, camera):
         print(f'Best run: #{self.best_run_id}; average MSE = {self.dof_mse:.2E}')
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from configuration import Config
 from Filter import Simulator
 
+from scipy.optimize import basinhopping
+
 ## initialise objects
 config          = Config()
 kf, camera, _   = config.init_filter_objects()
 
 ## simulation runs
 sim = Simulator(config)
-sim.run(kf, camera)
+sim.optimise(kf, camera)
+# # sim.run(kf, camera)
 
-## results
-sim.save_and_plot(camera)
+# ## results
+# sim.save_and_plot(camera)
