f2832722c0131c831d9d980cca6655affe65f867
sal <salehahr@users.noreply.github.com>
2021-09-01 11:37:10 +0200

    corrected rotations

--- Filter/Filter.py
+++ Filter/Filter.py
@@ -309,7 +309,7 @@ class Filter(object):
 
         # correct virtual SLAM reading to physical SLAM
         notch_quat = Quaternion(val=np.array([0, 0, ang_notch]), euler='xyz')
-        cam_rot_corrected = camera.qrot * notch_quat
+        cam_rot_corrected = notch_quat * camera.qrot
 
         # compute error state
         res_p_cam = camera.pos.reshape((3,)) - self.states.p_cam.reshape((3,))
