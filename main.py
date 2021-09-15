11881b9a27f22738cb8bcf956f289d95a8fdbf8f
sal <salehahr@users.noreply.github.com>
2021-09-15 11:23:33 +0200

    not doing multiple runs anymore; Simulator.run_once()

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ if config.mode == 'tune':
     sim.optimise()
 else:
     sim.show_run_progress = False
-    sim.run(disp_config=True, save_best=False)
+    sim.run_once()
+    # sim.run(disp_config=True, save_best=False)
     sim.save_and_plot(compact=True)
