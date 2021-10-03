3336003d3af2de701be0bff9b5268b9ae76617c1
sal <salehahr@users.noreply.github.com>
2021-10-03 21:25:35 +0200

    changed tuning params

--- configuration.py
+++ configuration.py
@@ -31,7 +31,7 @@ probe = SimpleProbe(scope_length=scope_length,
 # Camera parameters / R
 """ Noise """
 STDEV_PC            = [0.5, 0.5, 0.5]                   # [cm]
-STDEV_Q_DEG         = [0.1, 0.5, 0.5]        # [deg]
+STDEV_Q_DEG         = [0.01, 0.5, 0.5]        # [deg]
 STDEV_NOTCH_deg     = 0.001                              # [deg]
 
 STDEV_RC        = np.deg2rad(STDEV_Q_DEG)               # [rad]
