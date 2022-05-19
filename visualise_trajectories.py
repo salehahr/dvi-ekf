4b114bc0e9ae303260ecef824c6022576407d77a
s <salehahr@users.noreply.github.com>
2022-05-19 17:32:09 +0200

    wrapper function for interpolating SE3 group

--- visualise_trajectories.py
+++ visualise_trajectories.py
@@ -5,6 +5,7 @@ Script to visualise camera and IMU trajectories.
 from Models import create_camera
 from Models.Probe import TestProbe
 from tools import Config
+from tools.spatial import interpolate
 
 # config
 config = Config("config.yaml")
@@ -18,4 +19,7 @@ Wb_F_Wc = probe.T
 Wb_F_C = Wb_F_Wc * camera.frames
 Wb_F_B = Wb_F_C * probe.T.inv()
 
-probe.plot(block=True, camera_frames=Wb_F_C, imu_frames=Wb_F_B, animate=True)
+n_interframe_vals = 5
+imu_frames_interp = interpolate(Wb_F_B, n_interframe_vals)
+
+probe.plot(block=True, camera_frames=Wb_F_C, imu_frames=imu_frames_interp, animate=True)
