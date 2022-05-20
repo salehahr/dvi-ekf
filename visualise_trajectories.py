091aa881850d3006c738e01fd0f5606487c765bb
s <salehahr@users.noreply.github.com>
2022-05-20 09:29:55 +0200

    add function to plot and reconstruct imu data

--- visualise_trajectories.py
+++ visualise_trajectories.py
@@ -2,11 +2,13 @@
 Script to visualise camera and IMU trajectories.
 """
 import numpy as np
+from scipy.integrate import cumtrapz
 from spatialmath import UnitQuaternion
 
 from Models import create_camera
 from Models.Probe import TestProbe
 from tools import Config
+from tools.plots import plot_imu
 from tools.spatial import get_omega_local, interpolate
 
 # config
@@ -30,5 +32,26 @@ v = np.gradient(p, t_interp, axis=0)
 a = np.gradient(v, t_interp, axis=0)
 imu_q_eul = UnitQuaternion(imu_frames_interp).eul()
 om_B_local = get_omega_local(imu_frames_interp)
+
+v_recon = cumtrapz(a, t_interp, axis=0, initial=0) + v[0, :]
+p_recon = cumtrapz(v_recon, t_interp, axis=0, initial=0) + p[0, :]
+
+
+dq = UnitQuaternion.Alloc(len(imu_frames_interp))
+for i in range(len(imu_frames_interp)):
+    try:
+        dq[i] = UnitQuaternion.EulerVec(om_B_local[i])
+    except TypeError:
+        dq[i] = UnitQuaternion()
+    except Exception as e:
+        raise Exception
+
+q_recon = UnitQuaternion.Alloc(len(imu_frames_interp))
+q_recon[0] = UnitQuaternion.Eul(imu_q_eul[0, :])
+for i in range(1, len(imu_frames_interp)):
+    q_recon[i] = q_recon[i - 1] * dq[i - 1]
+eul_recon = q_recon.eul()
+
+plot_imu(t_interp, p, v, a, imu_q_eul, om_B_local, p_recon, v_recon, eul_recon)
 
 # probe.plot(block=True, camera_frames=Wb_F_C, imu_frames=imu_frames_interp, animate=True)
